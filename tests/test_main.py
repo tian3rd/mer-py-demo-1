167587f338ba7300c8630d5d8c2133d573dd0884
Phillip Wu <phillip.wu@seeingmachines.com>
2024-03-01 10:46:04 +1100

    This should fail

--- tests/test_main.py
+++ tests/test_main.py
@@ -5,7 +5,7 @@ from main import add_two
 
 class TestMain(unittest.TestCase):
     def test_add_two(self):
-        self.assertEqual(add_two(1, 2), 3)
+        self.assertEqual(add_two(1, 2), 4)
 
     def test_add_answer(self):
         self.assertEqual(add_two(1, answer), 43)
