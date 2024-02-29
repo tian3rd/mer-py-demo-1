191c90cb346bb1ce95d733f0ee4eabf0055fb99b
Phillip Wu <phillip.wu@seeingmachines.com>
2024-03-01 10:56:36 +1100

    Test github action

--- tests/test_main.py
+++ tests/test_main.py
@@ -5,6 +5,7 @@ from main import add_two
 
 class TestMain(unittest.TestCase):
     def test_add_two(self):
+        """Add two.."""
         self.assertEqual(add_two(1, 2), 3)
 
     def test_add_answer(self):
