375820ce31ace6acec3b68df019c711981b5c0d2
Phillip Wu <phillip.wu@seeingmachines.com>
2024-03-01 10:57:57 +1100

    Break the test

--- tests/test_main.py
+++ tests/test_main.py
@@ -9,7 +9,7 @@ class TestMain(unittest.TestCase):
         self.assertEqual(add_two(1, 2), 3)
 
     def test_add_answer(self):
-        self.assertEqual(add_two(1, answer), 43)
+        self.assertEqual(add_two(1, answer), 2)
 
 
 if __name__ == "__main__":
