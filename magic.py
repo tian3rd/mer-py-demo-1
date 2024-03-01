f68c6c54b0126c27afde8e12e627b460344fda6d
Phillip Wu <phillip.wu@seeingmachines.com>
2024-03-01 11:23:55 +1100

    Rename answer

--- magic.py
+++ magic.py
@@ -1,4 +1,4 @@
 """Magic number"""
 
 # answer to life
-answer = 42
+answer_to_life = 42
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,5 +1,5 @@
 import unittest
-from magic import answer
+from magic import answer_to_life
 from main import add
 
 
@@ -8,7 +8,7 @@ class TestMain(unittest.TestCase):
         self.assertEqual(add(1, 2), 3)
 
     def test_add_answer(self):
-        self.assertEqual(add(1, answer), 43)
+        self.assertEqual(add(1, answer_to_life), 43)
 
 
 if __name__ == "__main__":
