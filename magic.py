9f4c5f7be1ecaae8b6db9377dc74a954f9fd0d72
Phillip Wu <phillip.wu@seeingmachines.com>
2024-03-01 11:35:16 +1100

    Introduce a breaking change

--- magic.py
+++ magic.py
@@ -1,4 +1,4 @@
 """Magic number"""
 
 # answer to life
-answer = 42
+answer = 420
--- tests/test_main.py
+++ tests/test_main.py
@@ -8,7 +8,7 @@ class TestMain(unittest.TestCase):
         self.assertEqual(add(1, 2), 3)
 
     def test_add_answer(self):
-        self.assertEqual(add(1, answer), 43)
+        self.assertEqual(add(1, answer), 421)
 
 
 if __name__ == "__main__":
